5351760a8eb305ec0f259e4b7d4af393deee8b12
RyanWalton0410 <32870928+RyanWalton0410@users.noreply.github.com>
2017-10-17 10:14:22 -0400

    initial commit
    Test app

---
+++ testApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Please tell me your name");
+
+            string name = (Console.ReadLine());
+            Console.WriteLine("Is " + name + " ok?");
+            Console.WriteLine("\n(Y)es\n(N)o");
+
+           
+           
+            var ansys = Console.ReadKey();
+            
+            Console.WriteLine(name + "You're on a mission.. Do you want to hear more?");
+            Console.WriteLine("\n(Y)es\n(N)o");
+
+            if (ansys.KeyChar == 'y' || ansys.KeyChar == 'Y')
+            {
+                //Handle yes case
+                Console.Write("");
+                
+            }
+            if (ansys.KeyChar == 'n' || ansys.KeyChar == 'N')
+            {
+                //Handle no case
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Enter letters only");
+            }
+
+       
+        }
+
+    }
+}
+
