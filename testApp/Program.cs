cd06978ecd9de8bcd72d4d88c0b9c9eb1e1192a1
Ryan Walton <32870928+RyanWalton0410@users.noreply.github.com>
2017-10-18 09:48:38 -0400

    commit
    Added dialogue and ReadKeys to pause app

--- testApp/Program.cs
+++ testApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,28 +9,28 @@ namespace testApp
     class Program
     {
         static void Main(string[] args)
-
         {
 
             Console.WriteLine("Please tell me your name");
 
             string name = (Console.ReadLine());
-            
+
             Console.WriteLine("Is " + name + " ok?");
             Console.WriteLine("\n(Y)es\n(N)o");
 
-           
-           
+
+            //Variable for KeyChar
             var ansys = Console.ReadKey();
-            
-            Console.WriteLine(name + "You're on a mission.. Do you want to hear more?");
+
+            Console.WriteLine("Okay " + name + " You're on a mission.. Do you want to hear more?");
             Console.WriteLine("\n(Y)es\n(N)o");
 
             if (ansys.KeyChar == 'y' || ansys.KeyChar == 'Y')
             {
                 //Handle yes case
-                Console.WriteLine("");
-                
+                Console.WriteLine("Sweet!, Okay so I know you're blind an all so i'll describe where we are at.. we are stuck on a spaceship and we need your help");
+                Console.ReadKey();
+
             }
             if (ansys.KeyChar == 'n' || ansys.KeyChar == 'N')
             {
@@ -43,7 +43,7 @@ namespace testApp
                 Console.WriteLine("Enter letters only");
             }
 
-       
+
         }
 
     }
