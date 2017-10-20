9fc7a6e99ec595cd706e7c0238614e953b97cd86
Ryan Walton <32870928+RyanWalton0410@users.noreply.github.com>
2017-10-20 11:56:42 -0400

    Update 0.1.2
    Added story

--- testApp/Program.cs
+++ testApp/Program.cs
@@ -28,14 +28,26 @@ namespace testApp
             if (ansys.KeyChar == 'y' || ansys.KeyChar == 'Y')
             {
                 //Handle yes case
-                Console.WriteLine("Sweet!, Okay so I know you're blind an all so i'll describe where we are at.. we are stuck on a spaceship and we need your help");
+                Console.WriteLine("Sweet!, Okay so I know you're blind an all so i'll describe where we are at.. we are stuck on a spaceship and we need your help do you accept?");
                 Console.ReadKey();
-
+                
+                if(ansys.KeyChar == 'y' || ansys.KeyChar == 'Y')
+                {
+                    //handles yes case for the previous initial yes case 
+                    Console.WriteLine("Okay so I don't know where we need to go the door to the left for Right")    
+                    Console.WriteLine("\n(1)Left \n(2)Right);
+                    Console.ReadKey();
             }
             if (ansys.KeyChar == 'n' || ansys.KeyChar == 'N')
             {
                 //Handle no case
-                Console.WriteLine("");
+                Console.WriteLine("Cmon sir we really need you to help us.. it's.. I need help sir.. please ");
+                
+                if(ansys.KeyChar == 'n' || ansys.KeyChar == 'N')
+                {
+                    Console.WriteLine("Fine have it your way");
+                    System.Threading.Thread.Sleep(5000);
+                    this.Close();
             }
             else
             {
@@ -43,9 +55,8 @@ namespace testApp
                 Console.WriteLine("Enter letters only");
             }
 
-
+                }
+            }
         }
-
     }
 }
-
