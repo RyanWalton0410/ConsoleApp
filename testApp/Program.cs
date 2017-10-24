e2cac327693c8f8478850f31c2c3ae8a01a42477
Ryan Walton <32870928+RyanWalton0410@users.noreply.github.com>
2017-10-24 11:47:01 -0400

    Update
    More story

--- testApp/Program.cs
+++ testApp/Program.cs
@@ -16,35 +16,49 @@ namespace testApp
 			Console.WriteLine("\n(Y)es\n(N)o");
 			//Variable for KeyChar
 			var ansys = Console.ReadKey();
+			
 			Console.WriteLine("Okay " + name + " You're on a mission.. Do you want to hear more?");
 			Console.WriteLine("\n(Y)es\n(N)o");
-			if (ansys.KeyChar == 'y' || ansys.KeyChar == 'Y')
+if (ansys.KeyChar == 'y' || ansys.KeyChar == 'Y')
 			{
 				//Handle yes case
 				Console.WriteLine("Sweet!, Okay so I know you're blind an all so i'll describe where we are at.. we are stuck on a spaceship and we need your help do you accept?");
 				Console.ReadKey();
-				if (ansys.KeyChar == 'y' || ansys.KeyChar == 'Y')
+			if (ansys.KeyChar == 'y' || ansys.KeyChar == 'Y')
 				{
 					//handles yes case for the previous initial yes case 
 					Console.WriteLine("Okay so I don't know where we need to go the door to the left for Right");
 					Console.WriteLine("\n(1)Left \n(2)Right");
 					Console.ReadKey();
-					if (ansys.KeyChar == '1')
+				if (ansys.KeyChar == '1')
 					{
-						Console.WriteLine("This looks very... ominous, I dont like this");
+					  Console.WriteLine("This looks very... ominous, I dont like this");
+					  Console.WriteLine("\n(Y)es \n(N)o");
 					}
-					else
+					
+						if(ansys.KeyChar == 'y' || ansys.KeyChar == 'Y')
+						{
+						
+						}
+						if(ansys.KeyChar == 'n' || ansys.KeyChat == 'N')
+						{
+							
+						}
+						
+					
+					if(ansys.KeyChar == 'n' || ansys.KeyChar == 'N')
 					{
                        				 //Handles No case with an else method
 						Console.WriteLine("it's Bright... almost to bright I dont like this");
+						Console.WriteLine("\n(Y)es \n(N)o");
 					}
 				}
 
-				if (ansys.KeyChar == 'n' || ansys.KeyChar == 'N')
+			if (ansys.KeyChar == 'n' || ansys.KeyChar == 'N')
 				{
 					//Handle no case
 					Console.WriteLine("Cmon sir we really need you to help us.. it's.. I need help sir.. please ");
-					if (ansys.KeyChar == 'n' || ansys.KeyChar == 'N')
+	if (ansys.KeyChar == 'n' || ansys.KeyChar == 'N')
 					{
                         //Quit cheating.
 						Console.WriteLine("Fine have it your way");
